686826d7049da39e0503b9e9b7d6ec628b6edf1b
Jason Rupright <jasonsmr@hotmail.com>
2019-04-02 19:55:11 -0400

    Initial commit

---
+++ src/SortByPhoneNumber.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class SortByPhoneNumber implements Comparator<Student> {
+
+	@Override
+	public int compare (Student o1, Student o2) {
+
+		// TODO: Complete this function so that it allows
+		// Collections.sort to sort Students by their phone number
+		
+		return 0;
+	}
+}
